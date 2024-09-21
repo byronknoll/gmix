16134b25e8234c1db9cb6aafc75b6986a627be2c
Byron Knoll <byron@byronknoll.com>
2024-09-20 22:18:52 -0700

    refactoring

--- src/models/lstm-model.cpp
+++ src/models/lstm-model.cpp
@@ -6,7 +6,7 @@ LstmModel::LstmModel(ShortTermMemory& short_term_memory,
                      LongTermMemory& long_term_memory)
     : lstm_(0, 256, 20, 1, 100, 0.03, 10),
       top_(255),
-      mid_(0),
+      mid_(127),
       bot_(0),
       probs_(1.0 / 256, 256) {
   prediction_index_ = short_term_memory.num_predictions++;
@@ -15,20 +15,20 @@ LstmModel::LstmModel(ShortTermMemory& short_term_memory,
 void LstmModel::Predict(ShortTermMemory& short_term_memory,
                         const LongTermMemory& long_term_memory) {
   if (short_term_memory.recent_bits == 1) {
+    // A new byte has been observed. Update the byte-level predictions.
+    probs_ = lstm_.Predict(short_term_memory.last_byte);
     top_ = 255;
     bot_ = 0;
-    probs_ = lstm_.Predict(short_term_memory.last_byte);
   } else {
-    mid_ = bot_ + ((top_ - bot_) / 2);
     if (short_term_memory.new_bit) {
       bot_ = mid_ + 1;
     } else {
       top_ = mid_;
     }
   }
-  auto mid = bot_ + ((top_ - bot_) / 2);
-  float num = std::accumulate(&probs_[mid + 1], &probs_[top_ + 1], 0.0f);
-  float denom = std::accumulate(&probs_[bot_], &probs_[mid + 1], num);
+  mid_ = bot_ + ((top_ - bot_) / 2);
+  float num = std::accumulate(&probs_[mid_ + 1], &probs_[top_ + 1], 0.0f);
+  float denom = std::accumulate(&probs_[bot_], &probs_[mid_ + 1], num);
   float p = 0.5;
   if (denom != 0) p = num / denom;
   short_term_memory.SetPrediction(p, prediction_index_);
@@ -39,6 +39,7 @@ void LstmModel::Learn(const ShortTermMemory& short_term_memory,
   int current_byte =
       short_term_memory.recent_bits * 2 + short_term_memory.new_bit;
   if (current_byte >= 256) {
+    // A new byte has been observed.
     current_byte -= 256;
     lstm_.Perceive(current_byte);
   }
